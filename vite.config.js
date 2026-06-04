2bb3b7296f0d181a0ab9bb41636b8dcf25d687cd
Shahjalal Rafi <shahjalal@cottagehomecare.com>
2026-06-04 21:06:48 +0600

    Vite config update

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: [
+      'codeabc.org',
+      'api.codeabc.org'
+    ]
+  }
 })
